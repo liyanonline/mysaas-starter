287ee844d30b6769c494c18b86207578351ee058
liyanonline <liyanonline@gmail.com>
2025-06-16 14:59:37 +0800

    layout session

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 // // only work on client side:
 // import ScrollToTop from "@/components/ScrollToTop";
-// import { SessionProvider } from "next-auth/react";
+import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import ToasterContext from "./api/contex/ToasetContex";
 
