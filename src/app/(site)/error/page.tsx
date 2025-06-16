1f28c6d7cb6b0dd6019081b1893e07642df9e61b
liyanonline <liyanonline@gmail.com>
2025-06-16 15:07:51 +0800

    !meta

--- src/app/(site)/error/page.tsx
+++ src/app/(site)/error/page.tsx
@@ -4,11 +4,13 @@ import { SessionProvider } from "next-auth/react";
 
 import Breadcrumb from "@/components/Common/Breadcrumb";
 import NotFound from "@/components/NotFound";
-import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "404 Page | Play SaaS Starter Kit and Boilerplate for Next.js",
-};
+// // Error:   x You are attempting to export "metadata" from a component marked with "use client", which is disallowed. 
+// import { Metadata } from "next";
+
+// export const metadata: Metadata = {
+//   title: "404 Page | Play SaaS Starter Kit and Boilerplate for Next.js",
+// };
 
 const ErrorPage = () => {
   return (
