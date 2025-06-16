3e3047be60b786479d7364615214df1848d6594f
liyanonline <liyanonline@gmail.com>
2025-06-16 13:44:36 +0800

    !use client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-"use client";
+// "use client";
 
 import './globals.css';
 import type { Metadata, Viewport } from 'next';
@@ -15,10 +15,10 @@ import ToasterContext from "./api/contex/ToasetContex";
 
 
 
-// export const metadata: Metadata = {
-//   title: 'Next.js SaaS Starter',
-//   description: 'Get started quickly with Next.js, Postgres, and Stripe.'
-// };
+export const metadata: Metadata = {
+  title: 'Next.js SaaS Starter',
+  description: 'Get started quickly with Next.js, Postgres, and Stripe.'
+};
 
 export const viewport: Viewport = {
   maximumScale: 1
