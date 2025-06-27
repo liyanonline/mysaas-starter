0c95cd7ed645d7067c5d92134dd9d172e4afc929
liyanonline <liyanonline@gmail.com>
2025-06-27 08:21:47 +0800

    rrstor

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -104,38 +104,38 @@ function Header() {
   );
 }
 
-// export default function Layout({ children }: { children: React.ReactNode }) {
-//   return (
-//     <section className="flex flex-col min-h-screen">
-//       <Header />
-//       {children}
-//     </section>
-//   );
-// }
-export default async function DashboardLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  // Check if user has plan selected. If not redirect to subscibe
-  const supabase = createClient()
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser()
-
-  // check user plan in db
-  const checkUserInDB = await db.select().from(usersTable).where(eq(usersTable.email, user!.email!))
-  if (checkUserInDB[0].plan === "none") {
-    console.log("User has no plan selected")
-    return redirect('/subscribe')
-  }
-
-
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <DashboardHeader />
+    <section className="flex flex-col min-h-screen">
+      <Header />
       {children}
-    </html>
+    </section>
   );
 }
+// export default async function DashboardLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   // Check if user has plan selected. If not redirect to subscibe
+//   const supabase = createClient()
+
+//   const {
+//     data: { user },
+//   } = await supabase.auth.getUser()
+
+//   // check user plan in db
+//   const checkUserInDB = await db.select().from(usersTable).where(eq(usersTable.email, user!.email!))
+//   if (checkUserInDB[0].plan === "none") {
+//     console.log("User has no plan selected")
+//     return redirect('/subscribe')
+//   }
+
+
+//   return (
+//     <html lang="en">
+//       <DashboardHeader />
+//       {children}
+//     </html>
+//   );
+// }
