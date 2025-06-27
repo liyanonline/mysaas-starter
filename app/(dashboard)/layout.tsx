ccafec3f1899f0038eeaf67e91333640165f64bb
liyanonline <liyanonline@gmail.com>
2025-06-27 08:13:18 +0800

    more header

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import DashboardHeader from "@/components/DashboardHeader";
 import Link from 'next/link';
 import { use, useState, Suspense } from 'react';
 import { Button } from '@/components/ui/button';
@@ -103,11 +104,38 @@ function Header() {
   );
 }
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+// export default function Layout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <section className="flex flex-col min-h-screen">
+//       <Header />
+//       {children}
+//     </section>
+//   );
+// }
+export default async function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  // Check if user has plan selected. If not redirect to subscibe
+  const supabase = createClient()
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  // check user plan in db
+  const checkUserInDB = await db.select().from(usersTable).where(eq(usersTable.email, user!.email!))
+  if (checkUserInDB[0].plan === "none") {
+    console.log("User has no plan selected")
+    return redirect('/subscribe')
+  }
+
+
   return (
-    <section className="flex flex-col min-h-screen">
-      <Header />
+    <html lang="en">
+      <DashboardHeader />
       {children}
-    </section>
+    </html>
   );
 }
