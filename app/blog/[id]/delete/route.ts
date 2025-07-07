b2dc96f46d6a9f83b01b24898e575c3cf13592e1
liyanonline <liyanonline@gmail.com>
2025-07-07 08:16:31 +0800

    nc

--- app/blog/[id]/delete/route.ts
+++ app/blog/[id]/delete/route.ts
@@ -1,12 +1,30 @@
 import { deletePost } from '@/app/actions/blog';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(
-    request: Request,
-    context: { params: { id: string } }) {
-    const result = await deletePost(parseInt(context.params.id));
+    req: NextRequest,
+    context: { params: { id: string } }
+) {
+    const id = context.params.id;
+    const result = await deletePost(parseInt(id));
+
     if (result.error) {
         return NextResponse.json({ error: result.error }, { status: 400 });
     }
+
     return NextResponse.json({ success: true });
 }
+
+
+// import { deletePost } from '@/app/actions/blog';
+// import { NextResponse } from 'next/server';
+
+// export async function POST(
+//     request: Request,
+//     context: { params: { id: string } }) {
+//     const result = await deletePost(parseInt(context.params.id));
+//     if (result.error) {
+//         return NextResponse.json({ error: result.error }, { status: 400 });
+//     }
+//     return NextResponse.json({ success: true });
+// }
