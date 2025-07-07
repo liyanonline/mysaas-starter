07d6f2915f44024b5fda44ca6e8487bc6d0d7075
liyanonline <liyanonline@gmail.com>
2025-07-07 08:24:10 +0800

    nncc

--- app/blog/[id]/delete/route.ts
+++ app/blog/[id]/delete/route.ts
@@ -1,11 +1,13 @@
 import { deletePost } from '@/app/actions/blog';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST(
-    req: NextRequest,
-    context: { params: { id: string } }
-) {
-    const id = context.params.id;
+// Define the expected type for the context parameter
+interface Context {
+    params: { id: string };
+}
+
+export async function POST(req: NextRequest, { params }: Context) {
+    const id = params.id;
     const result = await deletePost(parseInt(id));
 
     if (result.error) {
@@ -15,16 +17,33 @@ export async function POST(
     return NextResponse.json({ success: true });
 }
 
-
 // import { deletePost } from '@/app/actions/blog';
-// import { NextResponse } from 'next/server';
+// import { NextRequest, NextResponse } from 'next/server';
 
 // export async function POST(
-//     request: Request,
-//     context: { params: { id: string } }) {
-//     const result = await deletePost(parseInt(context.params.id));
+//     req: NextRequest,
+//     context: { params: { id: string } }
+// ) {
+//     const id = context.params.id;
+//     const result = await deletePost(parseInt(id));
+
 //     if (result.error) {
 //         return NextResponse.json({ error: result.error }, { status: 400 });
 //     }
+
 //     return NextResponse.json({ success: true });
 // }
+
+
+// // import { deletePost } from '@/app/actions/blog';
+// // import { NextResponse } from 'next/server';
+
+// // export async function POST(
+// //     request: Request,
+// //     context: { params: { id: string } }) {
+// //     const result = await deletePost(parseInt(context.params.id));
+// //     if (result.error) {
+// //         return NextResponse.json({ error: result.error }, { status: 400 });
+// //     }
+// //     return NextResponse.json({ success: true });
+// // }
